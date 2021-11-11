80d63ff8ffb1b5a5341358bb1715cc6d67c0ea81
mykolao <mykolaoleskiv@gmail.com>
2021-11-12 00:49:20 +0200

    comment previous implementation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,22 +4,22 @@ import Button from "./Button/Button";
 import Label from "./Label";
 
 function App() {
-  const [state, setState] = useState(0);
-  const [minState, maxState] = [0, 5];
+  // const [state, setState] = useState(0);
+  // const [minState, maxState] = [0, 5];
 
-  const incrementHandler = () => setState(state + 1);
-  const resetHandler = () => setState(0);
+  // const incrementHandler = () => setState(state + 1);
+  // const resetHandler = () => setState(0);
 
-  const labelColor: "red" | "blue" =
-    state === maxState ? "red" : "blue";
+  // const labelColor: "red" | "blue" =
+  //   state === maxState ? "red" : "blue";
 
-  const incButton: boolean = state < maxState;
+  // const incButton: boolean = state < maxState;
 
-  const resButton: boolean = state > minState;
+  // const resButton: boolean = state > minState;
 
   return (
     <div className="app-container">
-      <div className="app">
+      {/* <div className="app">
         <div className="section label">
           <Label value={state} color={labelColor} />
         </div>
@@ -35,7 +35,7 @@ function App() {
             onClick={resetHandler}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
