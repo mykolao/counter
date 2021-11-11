a5390a95af300518ed41dd81f4d14710bd03723e
mykolao <mykolaoleskiv@gmail.com>
2021-11-12 00:59:51 +0200

    moved Counter functionality to Counter comp

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,7 +1,41 @@
-
+import { useState } from "react";
+import Button from "../Button";
+import Label from "../Label";
 
 const Counter = () => {
-  return <div className="block"></div>;
+  const [state, setState] = useState(0);
+  const [minState, maxState] = [0, 5];
+
+  const incrementHandler = () => setState(state + 1);
+  const resetHandler = () => setState(0);
+
+  const labelColor: "red" | "blue" =
+    state === maxState ? "red" : "blue";
+
+  const incButton: boolean = state < maxState;
+
+  const resButton: boolean = state > minState;
+  return (
+    <div className="block">
+      <div className="section main-panel">
+          <Label value={state} color={labelColor} />
+        </div>
+        <div className="section control-panel">
+          <Button
+            value="Incr"
+            disabled={!incButton}
+            onClick={incrementHandler}
+          />
+          <Button
+            value="Reset"
+            disabled={!resButton}
+            onClick={resetHandler}
+          />
+        </div>
+      {/* <div className="section"></div>
+      <div className="section control-panel"></div> */}
+    </div>
+  );
 };
 
 export default Counter;
