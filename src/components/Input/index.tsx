ff2a67bbaf233456f6db24799c44bf1d5fc07472
mykolao <mykolaoleskiv@gmail.com>
2021-11-12 02:00:52 +0200

    presentation completed

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,28 @@
-const Input = () => {
-  return <input className="input" type="number" />;
+import { ChangeEvent } from "react";
+
+export enum inputStatus {
+  good = "good",
+  bad = "bad",
+}
+interface props {
+  value: number;
+  status: inputStatus;
+  onValueChange: (v: number) => void;
+}
+
+const Input = ({ value, status, onValueChange }: props) => {
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>
+    onValueChange(Number(e.target.value));
+
+  return (
+    <input
+      min={0}
+      className={`input ${status}`}
+      value={value}
+      type="number"
+      onChange={onChangeHandler}
+    />
+  );
 };
 
 export default Input;
