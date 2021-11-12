8a9a49ccad242f8f4902ab7a73b640e3f67bd854
mykolao <mykolaoleskiv@gmail.com>
2021-11-12 02:00:11 +0200

    presentation completed

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import Button from "../Button";
+import Input, { inputStatus } from "../Input";
+
 const Settings = () => {
-  return <div className="block"></div>;
+  const [state, setState] = useState<[number, number]>([0, 1]);
+  const [minInput, setMinInput] = useState<number>(0);
+  const [maxInput, setMaxInput] = useState<number>(0);
+
+  const setHandler = () => {};
+  const minInputChangeHandler = () => {};
+  const minInputStatus = inputStatus.good;
+
+  const maxInputChangeHandler = () => {};
+  const maxInputStatus = inputStatus.good;
+
+  const setButton = false;
+  return (
+    <div className="block">
+      <div className="section input-panel">
+        <div className="input-label">Min Value</div>
+        <Input
+          value={state[0]}
+          status={minInputStatus}
+          onValueChange={minInputChangeHandler}
+        />
+        <div className="input-label">Max Value</div>
+        <Input
+          value={state[1]}
+          status={maxInputStatus}
+          onValueChange={maxInputChangeHandler}
+        />
+      </div>
+      <div className="section control-panel">
+        <Button
+          disabled={!setButton}
+          value="Set"
+          onClick={setHandler}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
