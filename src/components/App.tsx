7c40fbb941f8c1351b7265ef540097507927b88d
mykolao <mykolaoleskiv@gmail.com>
2021-11-15 20:28:46 +0200

    rework app structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,22 +3,40 @@ import "./App.css";
 import Counter from "./Counter";
 import Settings, { CounterValues } from "./Settings";
 
+type CounterLimits = [number, number];
+type CounterStatus = "ok" | "set" | "error";
+interface CounterState {
+  value: number;
+  limits: CounterLimits;
+  status: CounterStatus;
+}
+
+const fetchState = (): CounterValues => {
+  const item = localStorage.getItem("counter-limits");
+
+  return item == null
+    ? [0, 1]
+    : item.split(",").map((n) => Number(n));
+};
+
+const saveState = (state: CounterValues) =>
+  localStorage.setItem("counter-limits", String(state));
+
 function App() {
-  const [state, setState] = useState<CounterValues>(() => {
-    const item = localStorage.getItem("counter-limits");
+  // const [state, setState] = useState<CounterValues>(fetchState);
 
-    return item == null
-      ? [1, 1]
-      : item.split(",").map((n) => Number(n));
-  });
-
-  const onSetHandler = (values: CounterValues) =>
-    setState(values);
-
+  // const onSetHandler = (values: CounterValues) => {
+  //   saveState(values);
+  //   setState(values);
+  // };
   return (
     <div className="app-container">
-      <Settings values={state} onSet={onSetHandler} />
-      <Counter values={state} status="set"/>
+      {/* <Settings values={state} onSet={onSetHandler} />
+      <Counter
+        values={state}
+        status="ok"
+        initialValue={state[0]}
+      /> */}
     </div>
   );
 }
