e5361f4c9e3a4204e4b9a975d5ef5f47bd42ee1c
mykolao <mykolaoleskiv@gmail.com>
2021-11-15 19:52:14 +0200

    added optional size property

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,10 +1,17 @@
 type props = {
-  value: number;
-  color: string;
+  value: number | string;
+  color: "red" | "blue";
+  size?: string;
 };
 
-const Label = ({ value, color }: props) => {
-  return <span className={color}>{value}</span>;
+const Label = ({ value, color, size }: props) => {
+  const style = size ? { fontSize: `${size}px` } : {};
+
+  return (
+    <span style={style} className={color}>
+      {value}
+    </span>
+  );
 };
 
 export default Label;
