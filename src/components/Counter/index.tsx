7e70466edd03ab47c3c934dfb4c1ba621df48402
mykolao <mykolaoleskiv@gmail.com>
2021-12-20 23:55:40 +0200

    Counter component completed

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,67 +1,83 @@
+import { useDispatch, useSelector } from "react-redux";
+import { State } from "../../redux/default-state";
+import { increment, reset } from "../../redux/reducer";
+import Button from "../Button";
+import Label from "../Label";
 
 const Counter = () => {
-  return <></>
-  // const [minValue, maxValue] = limits;
+  // State
+  const dispatch = useDispatch();
+  const state = useSelector<State, State>((state) => state);
+  const { min, max, value, status } = state;
+  const minValue = Number(min);
+  const maxValue = Number(max);
 
-  // const incrementHandler = () => onCounterChange(value + 1);
-  // const resetHandler = () => onCounterChange(minValue);
+  // Label Type Calculation
+  let label, incButton, resButton;
 
-  // let label, incButton, resButton;
+  let labelColor: "red" | "blue";
 
-  // let labelColor: "red" | "blue";
+  switch (status) {
+    case "set":
+      incButton = resButton = false;
 
-  // switch (status) {
-  //   case "set":
-  //     incButton = resButton = false;
+      label = (
+        <Label
+          size="32"
+          value={"Enter values & press set"}
+          color={"blue"}
+        />
+      );
 
-  //     label = (
-  //       <Label
-  //         size="32"
-  //         value={"Enter values & press set"}
-  //         color={"blue"}
-  //       />
-  //     );
+      break;
 
-  //     break;
+    case "ok":
+      labelColor = value === maxValue ? "red" : "blue";
+      incButton = value < maxValue && status === "ok";
+      resButton = value > minValue;
 
-  //   case "ok":
-  //     labelColor = value === maxValue ? "red" : "blue";
-  //     incButton = value < maxValue && status === "ok";
-  //     resButton = value > minValue;
+      label = <Label value={value} color={labelColor} />;
+      break;
 
-  //     label = <Label value={value} color={labelColor} />;
-  //     break;
+    case "error":
+      incButton = resButton = false;
 
-  //   case "error":
-  //     incButton = resButton = false;
+      label = (
+        <Label
+          size="24"
+          value={"Incorrect Values!"}
+          color={"red"}
+        />
+      );
+      break;
+  }
 
-  //     label = (
-  //       <Label
-  //         size="24"
-  //         value={"Incorrect Values!"}
-  //         color={"red"}
-  //       />
-  //     );
-  //     break;
-  // }
+  // Event Handling
+  const incrementHandler = () => {
+    dispatch(increment());
+  };
 
-  // return (
-  //   <div className="block">
-  //     <div className="section counter-panel">{label}</div>
-  //     <div className="section control-panel">
-  //       <Button
-  //         value="Incr"
-  //         disabled={!incButton}
-  //         onClick={incrementHandler}
-  //       />
-  //       <Button
-  //         value="Reset"
-  //         disabled={!resButton}
-  //         onClick={resetHandler}
-  //       />
-  //     </div>
-  //   </div>
-  // );
+  const resetHandler = () => {
+    dispatch(reset());
+  };
+
+  return (
+    <div className="block">
+      <div className="section counter-panel">{label}</div>
+      <div className="section control-panel">
+        <Button
+          value="Incr"
+          disabled={!incButton}
+          onClick={incrementHandler}
+        />
+        <Button
+          value="Reset"
+          disabled={!resButton}
+          onClick={resetHandler}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
