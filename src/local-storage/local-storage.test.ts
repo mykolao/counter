a455d9d3ba579a00957fc33f0c736d632a8165a1
mykolao <mykolaoleskiv@gmail.com>
2021-12-20 21:03:20 +0200

    LocalStorage compeleted

--- src/local-storage/local-storage.test.ts
+++ src/local-storage/local-storage.test.ts
@@ -1,10 +1,26 @@
-test("LocalStorage should be clean", () => {
-  localStorage.clear();
-  expect(localStorage.length).toBe(0);
+import defaultState, { State } from "../redux/default-state";
+import LocalStorage from "./local-storage";
+
+beforeEach(() => {
+  LocalStorage.clearState();
 });
 
-test("LocalStorage should have one key", () => {
-  localStorage.setItem("key", "value");
-  expect(localStorage.length).toBe(1);
-  
+test("LocalStorage should have default state", () => {
+  const state: State = LocalStorage.getState();
+  expect(state).toBe(defaultState);
+});
+
+test("LocalStorage should be changed", ()=> {
+  const input: State = {
+    min: 1,
+    max: 2,
+    value: 1,
+    status: "set"
+  }
+  LocalStorage.setState(input);
+
+  const result = LocalStorage.getState();
+  expect(result.max).toBe(2);
+  expect(result.value).toBe(1);
+  expect(result.status).toBe("set");
 })
--- src/local-storage/local-storage.ts
+++ src/local-storage/local-storage.ts
@@ -1,4 +1,28 @@
+import defaultState, { State } from "../redux/default-state";
 
-const LocalStorage = {};
+const KEY = "counter-state";
+
+const parseState = (text: string | null): State => {
+  return text ? JSON.parse(text) : defaultState;
+};
+
+const getState = (): State => {
+  return parseState(localStorage.getItem(KEY));
+};
+
+const setState = (state: State): void => {
+  let str = JSON.stringify(state);
+  localStorage.setItem(KEY, str);
+};
+
+const clearState = (): void => {
+  localStorage.removeItem(KEY);
+};
+
+const LocalStorage = {
+  getState,
+  setState,
+  clearState,
+};
 
 export default LocalStorage;
