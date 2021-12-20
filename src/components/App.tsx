0fa3bdaadaf29e090a9212ca1432bce789510df8
mykolao <mykolaoleskiv@gmail.com>
2021-12-20 21:20:46 +0200

    App rebuilt

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,61 +1,14 @@
-import { useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
 import Settings from "./Settings";
 
-// Types
-export type CounterLimits = number[];
-export type CounterStatus = "ok" | "set" | "error";
-export interface CounterState {
-  value: number;
-  limits: CounterLimits;
-  status: CounterStatus;
-}
-
-const getLimits = (): CounterLimits => {
-  const item = localStorage.getItem("counter-limits");
-
-  return item == null
-    ? [0, 1]
-    : item.split(",").map((n) => Number(n));
-};
-
-const setLimits = (state: CounterLimits) =>
-  localStorage.setItem("counter-limits", String(state));
-
-const limits = getLimits();
-const initialState: CounterState = {
-  limits,
-  value: limits[0],
-  status: "ok",
-};
-
-function App() {
-  const [state, setState] =
-    useState<CounterState>(initialState);
-
-  const onSetHandler = (limits: CounterLimits) => {
-    setLimits(limits);
-    setState({ value: limits[0], limits, status: "ok" });
-  };
-
-  const onChangeHandler = () => {
-    setState({ ...state, status: "set" });
-  };
-
-  const onErrorHandler = () => {
-    setState({ ...state, status: "error" });
-  };
-
-  const onCounterChangeHandler = (value: number) =>
-    setState({ ...state, value });
-
+const App = () => {
   return (
     <div className="app-container">
       <Settings />
       <Counter />
     </div>
   );
-}
+};
 
 export default App;
