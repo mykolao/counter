16c71789e3d9536ace538d0d97c1857d38ef69e1
mykolao <mykolaoleskiv@gmail.com>
2021-12-20 19:37:03 +0200

    initial state defined

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,3 +1,17 @@
+type Status = "ok" | "set" | "error";
+export interface State {
+  min: number;
+  max: number;
+  value: number;
+  status: Status;
+}
+
+const initialState: State = {
+  min: 0,
+  max: 1,
+  value: 0,
+  status: "ok",
+};
 
 const reducer = () => {};
 
