e3a5471eb3ac655c7145ffe7cca406bb9a0449fd
mykolao <mykolaoleskiv@gmail.com>
2021-12-20 23:32:52 +0200

    settings component completed

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,6 +1,100 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { State, Status } from "../../redux/default-state";
+import { setMax, setMin, setStatus } from "../../redux/reducer";
+import Button from "../Button";
+import Input from "../Input";
+
+interface SettingsState {
+  min: string;
+  max: string;
+  status: Status;
+}
 
 const Settings = () => {
-  return <></>
+  const dispatch = useDispatch();
+
+  // Global State
+  const state = useSelector<State, SettingsState>(
+    ({ min, max, status }) => ({
+      min,
+      max,
+      status,
+    })
+  );
+
+  console.log("Receieved State: ", state);
+
+  // Validation
+  const max = Number(state.max);
+  const min = Number(state.min);
+
+  const isMaxValid =
+    max > 0 && max > min && Boolean(state.max.length);
+  const isMinValid =
+    min >= 0 && min < max && Boolean(state.min.length);
+
+  const isValid = isMaxValid && isMinValid && max !== min;
+  if (state.status === "error" && isValid) {
+    dispatch(setStatus("set"));
+  } else if (state.status !== "error" && !isValid) {
+    dispatch(setStatus("error"));
+  }
+
+  const buttonStatus = state.status === "set" ? false : true;
+
+  // Event Handlers
+  const inputChangeHandler = (
+    value: string,
+    type: "max" | "min"
+  ) => {
+    const isInputValid = Boolean(
+      value.length && !isNaN(+value)
+    );
+    const statusAction = isInputValid
+      ? setStatus("set")
+      : setStatus("error");
+
+    let valueAction =
+      type === "max" ? setMax(value) : setMin(value);
+
+    dispatch(valueAction);
+    dispatch(statusAction);
+  };
+
+  const onButtonClickHandler = () => {
+    dispatch(setStatus("ok"));
+  };
+
+  return (
+    <div className="block">
+      <div className="section input-panel">
+        <div className="input-label">Max Value</div>
+        <Input
+          value={state.max}
+          valid={isMaxValid}
+          onValueChange={(v) => {
+            inputChangeHandler(v, "max");
+          }}
+        />
+        <div className="input-label">Min Value</div>
+        <Input
+          value={state.min}
+          valid={isMinValid}
+          onValueChange={(v) => {
+            inputChangeHandler(v, "min");
+          }}
+        />
+      </div>
+      <div className="section control-panel">
+        <Button
+          disabled={buttonStatus}
+          value="Set"
+          onClick={onButtonClickHandler}
+        />
+      </div>
+    </div>
+  );
   // const [state, setState] = useState<CounterLimits>(values);
   // const [min, max] = state;
 
@@ -34,25 +128,10 @@ const Settings = () => {
   // return (
   //   <div className="block">
   //     <div className="section input-panel">
-  //       <div className="input-label">Max Value</div>
-  //       <Input
-  //         value={state[1]}
-  //         status={maxInputStatus}
-  //         onValueChange={maxInputChangeHandler}
-  //       />
-  //       <div className="input-label">Min Value</div>
-  //       <Input
-  //         value={state[0]}
-  //         status={minInputStatus}
-  //         onValueChange={minInputChangeHandler}
-  //       />
+
   //     </div>
   //     <div className="section control-panel">
-  //       <Button
-  //         disabled={!setButton}
-  //         value="Set"
-  //         onClick={setHandler}
-  //       />
+
   //     </div>
   //   </div>
   // );
